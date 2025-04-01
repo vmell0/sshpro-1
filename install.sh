6d262a43e0e75de0968443d6c355c52c219706e4
vmell0 <152212726+vmell0@users.noreply.github.com>
2025-04-01 04:57:18 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
+sudo apt install wget -y
+sudo apt remove speedtest-cli -y; sudo rm /etc/apt/sources.list.d/ookla_speedtest-cli.list
+sudo apt intall curl -y
+sudo apt update -y && apt upgrade -y && apt autoremove -y && apt -f install -y && apt autoclean -y
+
 # Função para exibir o menu
 menu() {
     clear
@@ -8,11 +15,10 @@ menu() {
     echo "━━━━━━━"
     echo "[1] SSH-PRO"
     echo "[2] SSH-PRO BETA"
-    echo "[3] CORRIGIR ERRO UPDATE"
-    echo "[4] PAINEL DT"
-    echo "[5] LIMPAR VPS"
-    echo "[6] ALTERAR SENHA ROOT"
-    echo "[7] OTIMIZAR VPS"
+    echo "[3] PAINEL DT"
+    echo "[4] LIMPAR VPS"
+    echo "[5] ALTERAR SENHA ROOT"
+    echo "[6] OTIMIZAR VPS"
     echo "[0] Sair"
     echo "━━━━━━━"
     echo -n "Escolha uma opção: "
@@ -21,13 +27,12 @@ menu() {
 # Função para executar os comandos de acordo com a opção escolhida
 executar_opcao() {
     case $1 in
-        1) clear; bash <(curl -sL https://raw.githubusercontent.com/vmell0/pro/refs/heads/main/sshpro.sh) ;;
-        2) clear; bash <(curl -s https://raw.githubusercontent.com/vmell0/RustyManager/refs/heads/main/install.sh) ;;
-        3) clear; sudo apt remove speedtest-cli -y; sudo rm /etc/apt/sources.list.d/ookla_speedtest-cli.list ;;
-        4) clear; apt update; apt upgrade -y; apt install wget -y; bash <(wget -qO- https://raw.githubusercontent.com/vmell0/pro/refs/heads/main/dtmod.sh) ;;
-        5) clear; apt update -y && apt upgrade -y && apt autoremove -y && apt -f install -y && apt autoclean -y ;;
-        6) clear; bash <(curl -sL https://raw.githubusercontent.com/xzlordzx/sshpro/refs/heads/main/senharoot.sh) ;;
-        7) clear; bash <(curl -sL https://raw.githubusercontent.com/xzlordzx/sshpro/refs/heads/main/otimizar.sh) ;;
+        1) clear; bash <(curl -sL https://raw.githubusercontent.com/vmell0/pro/refs/heads/main/sshpro.sh); menu ;;
+        2) clear; bash <(curl -s https://raw.githubusercontent.com/vmell0/RustyManager/refs/heads/main/install.sh); menu ;;
+        3) clear; bash <(wget -qO- https://raw.githubusercontent.com/vmell0/pro/refs/heads/main/dtmod.sh) ;;
+        4) clear; apt update -y && apt upgrade -y && apt autoremove -y && apt -f install -y && apt autoclean -y ;;
+        5) clear; bash <(curl -sL https://raw.githubusercontent.com/xzlordzx/sshpro/refs/heads/main/senharoot.sh) ;;
+        6) clear; bash <(curl -sL https://raw.githubusercontent.com/xzlordzx/sshpro/refs/heads/main/otimizar.sh) ;;
         0) echo "Saindo..."; exit 0 ;;
         *) echo "Opção inválida!";;
     esac
